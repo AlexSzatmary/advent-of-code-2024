03b81c6993375ae2da319c519cbb84d0cdb6822f
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-12-04 21:46:23 -0500

    Solve day-03-2

---
+++ day-03/run_03_2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import re
+import sys
+import timeit
+from itertools import starmap
+
+
+def solve_without_re(program: str) -> int:
+    """
+    I got this solution first and I still think it's nice.
+    """
+    dont = 0
+    do = 0
+    total = 0
+    while do != -1:
+        dont = program.find("don't()", do)
+        total += sum(
+            starmap(
+                lambda a, b: int(a) * int(b),
+                re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", program[do:dont]),
+            )
+        )
+        do = program.find("do()", dont)
+    return total
+
+
+def solve(program: str) -> int:
+    program_scrubbed = re.sub("don't.*", "", re.sub(r"don't\(\).*?do\(\)", "", program))
+    return sum(
+        starmap(
+            lambda a, b: int(a) * int(b),
+            re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", program_scrubbed),
+        )
+    )
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    if argv[0] == "python":
+        argv = argv[1:]
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+    program = "".join(L).replace("\n", "")
+    start = timeit.default_timer()
+    print(solve(program))
+    stop = timeit.default_timer()
+    if "time" in argv:
+        print("Time:", stop - start)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ day-03/test_03_2.py
@@ -0,0 +1,15 @@
+import run_03_2
+import os
+
+inex_path = os.path.join(os.path.dirname(__file__), "inex-03-2.txt")
+with open(inex_path) as hin:
+    inex_1 = hin.readlines()
+program_1 = inex_1[0]
+
+
+def test_solve():
+    assert run_03_2.solve(program_1) == 48
+
+
+def test_solve_without_re():
+    assert run_03_2.solve_without_re(program_1) == 48

7dd81f91116ad26932abe657021eccc8c5a54306
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-12-14 22:25:27 -0500

    Solve day-09-2

--- day-09/run_09.py
+++ day-09/run_09.py
@@ -9,20 +9,20 @@ def parse(input_lines: list[str]) -> tuple[list[int], list[int]]:
     return list(map(int, input_lines[0][:-1:2])), list(map(int, input_lines[0][1:-1:2]))
 
 
-def move_files(block_sizes: list[int], gap_sizes: list[int]) -> Generator[int]:
+def move_files_1(file_sizes: list[int], gap_sizes: list[int]) -> Generator[int]:
     right_gen = (
-        len(block_sizes) - i - 1
-        for i, block_size in enumerate(reversed(block_sizes))
-        for _ in range(block_size)
+        len(file_sizes) - i - 1
+        for i, file_size in enumerate(reversed(file_sizes))
+        for _ in range(file_size)
     )
     k = 0
-    max_k = sum(block_sizes)
-    for block_id, (block_size, gap_size) in enumerate(zip(block_sizes, gap_sizes)):
-        for _ in range(block_size):
+    max_k = sum(file_sizes)
+    for file_id, (file_size, gap_size) in enumerate(zip(file_sizes, gap_sizes)):
+        for _ in range(file_size):
             k += 1
             if k > max_k:
                 break
-            yield block_id
+            yield file_id
         for _ in range(gap_size):
             k += 1
             if k > max_k:
@@ -30,8 +30,41 @@ def move_files(block_sizes: list[int], gap_sizes: list[int]) -> Generator[int]:
             yield next(right_gen)
 
 
-def solve_1(blocks: list[int], gaps: list[int]) -> int:
-    return sum(i * file_id for i, file_id in enumerate(move_files(blocks, gaps)))
+def solve_1(file_sizes: list[int], gap_sizes: list[int]) -> int:
+    return sum(
+        i * file_id for i, file_id in enumerate(move_files_1(file_sizes, gap_sizes))
+    )
+
+
+def move_files_2(
+    file_sizes: list[int], gap_sizes: list[int]
+) -> list[tuple[int, int, int]]:
+    files = []
+    files.append((0, file_sizes[0], 0))  # position, size, index
+    for index, (gap_size, file_size) in enumerate(zip(gap_sizes, file_sizes[1:]), 1):
+        files.append((files[-1][0] + files[-1][1] + gap_size, file_size, index))
+    moved_files = files.copy()
+    for file in reversed(files):
+        for i in range(len(moved_files) - 1):
+            if moved_files[i][2] == file[2]:
+                break  # found self
+            elif (
+                moved_files[i + 1][0] - (moved_files[i][0] + moved_files[i][1])
+                >= file[1]
+            ):
+                moved_files.remove(file)
+                new_file = ((moved_files[i][0] + moved_files[i][1]), file[1], file[2])
+                moved_files.insert(i + 1, new_file)
+                break
+    return moved_files
+
+
+def solve_2(blocks: list[int], gaps: list[int]) -> int:
+    moved_blocks = move_files_2(blocks, gaps)
+    count = 0
+    for position, size, index in moved_blocks:
+        count += size * (position + position + size - 1) // 2 * index
+    return count
 
 
 def main(argv: list[str] | None = None) -> None:
@@ -45,6 +78,8 @@ def main(argv: list[str] | None = None) -> None:
     start = timeit.default_timer()
     if "1" in argv:
         print(solve_1(blocks, gaps))
+    if "2" in argv:
+        print(solve_2(blocks, gaps))
     stop = timeit.default_timer()
     if "time" in argv:
         print("Time:", stop - start)
--- day-09/test_09.py
+++ day-09/test_09.py
@@ -1,5 +1,5 @@
 import pytest
-from run_09 import parse, move_files, solve_1
+from run_09 import parse, move_files_1, solve_1, move_files_2, solve_2
 import os
 
 
@@ -13,19 +13,33 @@ def setup() -> tuple[list[int], list[int]]:
 
 def test_parse(setup: tuple[list[int], list[int]]) -> None:
     blocks, gaps = setup
-    chars = [
-        str(i) * b + "." * g for i, (b, g) in enumerate(zip(blocks, gaps))
-    ]
+    chars = [str(i) * b + "." * g for i, (b, g) in enumerate(zip(blocks, gaps))]
     chars.append(str(len(blocks) - 1) * blocks[-1])
     assert "".join(chars) == "00...111...2...333.44.5555.6666.777.888899"
 
 
-def test_move_files(setup: tuple[list[int], list[int]]) -> None:
-    assert (
-        "".join(map(str, move_files(*setup)))
-        == "0099811188827773336446555566"
-    )
+def test_move_files_1(setup: tuple[list[int], list[int]]) -> None:
+    assert "".join(map(str, move_files_1(*setup))) == "0099811188827773336446555566"
 
 
 def test_solve_1(setup: tuple[list[int], list[int]]) -> None:
     assert solve_1(*setup) == 1928
+
+
+def test_move_files_2(setup: tuple[list[int], list[int]]) -> None:
+    moved_files = move_files_2(*setup)
+    rendered_blocks = []
+    for i in range(0, len(moved_files) - 1):
+        rendered_blocks.append(str(moved_files[i][2]) * moved_files[i][1])
+        rendered_blocks.append(
+            "." * (moved_files[i + 1][0] - (moved_files[i][0] + moved_files[i][1]))
+        )
+    rendered_blocks.append(str(moved_files[-1][2]) * moved_files[-1][1])
+    rendered_blocks.append("..")
+    assert "".join(
+        rendered_blocks
+    ) == "00992111777.44.333....5555.6666.....8888.."
+
+
+def test_solve_2(setup: tuple[list[int], list[int]]) -> None:
+    assert solve_2(*setup) == 2858

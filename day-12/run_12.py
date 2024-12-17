488552ff2906cb68b329614c7a9506b40d66664b
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-12-16 22:39:44 -0500

    Solve day-12-2

--- day-12/run_12.py
+++ day-12/run_12.py
@@ -21,7 +21,7 @@ def array_to_str(array: np.ndarray) -> str:
     )
 
 
-def garden_fill(garden: np.ndarray, i: int, j: int) -> tuple[np.ndarray, int, int]:
+def garden_fill(garden: np.ndarray, i: int, j: int) -> tuple[np.ndarray, int, int, int]:
     """
     Fills part of garden having single continuous plant type.
 
@@ -30,6 +30,7 @@ def garden_fill(garden: np.ndarray, i: int, j: int) -> tuple[np.ndarray, int, in
     garden : array with ints representing plant type. This gets modified.
     i : int index of row
     j : int index of column
+    n_side : number of sides of the plot
 
     Returns
     -------
@@ -43,6 +44,11 @@ def garden_fill(garden: np.ndarray, i: int, j: int) -> tuple[np.ndarray, int, in
     edges = [(i, j)]
     area = 1  # the starting square area
     perimeter = 0
+    n_side = 0
+    i_min = i
+    i_max = i
+    j_min = j
+    j_max = j
     while edges:
         next_edges = []
         for edge in edges:
@@ -53,30 +59,58 @@ def garden_fill(garden: np.ndarray, i: int, j: int) -> tuple[np.ndarray, int, in
                 elif garden[new_i, new_j] == starting_type:
                     next_edges.append((new_i, new_j))
                     region[new_i, new_j] = True
+                    i_min = min(i_min, new_i)
+                    i_max = max(i_max, new_i)
+                    j_min = min(j_min, new_j)
+                    j_max = max(j_max, new_j)
                     area += 1
                 else:
                     perimeter += 1
 
         edges = next_edges
+
+    for i in range(i_min - 1, i_max + 1):
+        for j in range(j_min - 1, j_max + 1):
+            plots_filled = np.sum(region[i : i + 2, j : j + 2])
+            if plots_filled == 1 or plots_filled == 3:
+                n_side += 1
+            elif plots_filled == 2 and region[i, j] == region[i + 1, j + 1]:
+                n_side += 2
+
     garden[region] = 0
-    return garden, area, perimeter
+    return garden, area, perimeter, n_side
 
 
-def tally_costs(garden: np.ndarray) -> dict[str, int]:
+def tally_costs_1(garden: np.ndarray) -> dict[str, int]:
     costs = defaultdict(lambda: 0)
     "Given garden, find costs per region"
     for i in range(1, garden.shape[0] - 1):
         for j in range(1, garden.shape[1] - 1):
             if garden[i, j]:
                 starting_type = chr(garden[i, j])
-                garden, area, perimeter = garden_fill(garden, i, j)
+                garden, area, perimeter, _ = garden_fill(garden, i, j)
                 costs[starting_type] += area * perimeter
     return costs
 
 
+def tally_costs_2(garden: np.ndarray) -> dict[str, int]:
+    costs = defaultdict(lambda: 0)
+    "Given garden, find costs per region"
+    for i in range(1, garden.shape[0] - 1):
+        for j in range(1, garden.shape[1] - 1):
+            if garden[i, j]:
+                starting_type = chr(garden[i, j])
+                garden, area, _, n_side = garden_fill(garden, i, j)
+                costs[starting_type] += area * n_side
+    return costs
+
+
 def solve_1(garden: np.ndarray) -> int:
-    return sum(tally_costs(garden).values())
-    # 1396526 is too low
+    return sum(tally_costs_1(garden).values())
+
+
+def solve_2(garden: np.ndarray) -> int:
+    return sum(tally_costs_2(garden).values())
 
 
 def main(argv: list[str] | None = None) -> None:
@@ -90,6 +124,8 @@ def main(argv: list[str] | None = None) -> None:
     start = timeit.default_timer()
     if "1" in argv:
         print(solve_1(stones))
+    if "2" in argv:
+        print(solve_2(stones))
     stop = timeit.default_timer()
     if "time" in argv:
         print("Time:", stop - start)
--- day-12/test_12.py
+++ day-12/test_12.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import numpy as np
 import os
 import pytest
-from run_12 import parse, garden_fill, tally_costs, solve_1, array_to_str
+from run_12 import parse, garden_fill, tally_costs_1, solve_1, array_to_str, solve_2
 
 
 def load(file_name: str) -> list[str]:
@@ -27,6 +27,16 @@ def garden_3() -> np.ndarray:
     return parse(load("inex-12-3.txt"))
 
 
+@pytest.fixture
+def garden_4() -> np.ndarray:
+    return parse(load("inex-12-4.txt"))
+
+
+@pytest.fixture
+def garden_5() -> np.ndarray:
+    return parse(load("inex-12-5.txt"))
+
+
 def test_parse(
     garden_1: np.ndarray, garden_2: np.ndarray, garden_3: np.ndarray
 ) -> None:
@@ -36,44 +46,53 @@ def test_parse(
 
 
 def test_garden_fill(
-    garden_1: np.ndarray, garden_2: np.ndarray, garden_3: np.ndarray
+    garden_1: np.ndarray, garden_2: np.ndarray, garden_4: np.ndarray
 ) -> None:
     garden = garden_1
     # A
-    garden, area, perimeter = garden_fill(garden, 1, 2)
+    garden, area, perimeter, n_side = garden_fill(garden, 1, 2)
     print(array_to_str(garden))
     assert np.all(garden[1, 1:-1] == 0)
     assert np.all(garden[2:] == garden_1[2:])
     assert area == 4
     assert perimeter == 10
+    assert n_side == 4
     # B
-    garden, area, perimeter = garden_fill(garden, 2, 2)
+    garden, area, perimeter, n_side = garden_fill(garden, 2, 2)
     assert area == 4
     assert perimeter == 8
+    assert n_side == 4
     # C
-    garden, area, perimeter = garden_fill(garden, 3, 4)
+    garden, area, perimeter, n_side = garden_fill(garden, 3, 4)
     assert area == 4
     assert perimeter == 10
+    assert n_side == 8
     # D
-    garden, area, perimeter = garden_fill(garden, 2, 4)
+    garden, area, perimeter, n_side = garden_fill(garden, 2, 4)
     assert area == 1
     assert perimeter == 4
+    assert n_side == 4
     # E
-    garden, area, perimeter = garden_fill(garden, 4, 3)
+    garden, area, perimeter, n_side = garden_fill(garden, 4, 3)
     assert area == 3
     assert perimeter == 8
+    assert n_side == 4
 
-    _, area, perimeter = garden_fill(garden_2, 1, 2)
+    _, area, perimeter, _ = garden_fill(garden_2, 1, 2)
     assert area == 21
     assert perimeter == 36
 
+    _, area, _, n_side = garden_fill(garden_4, 1, 1)
+    assert area == 17
+    assert n_side == 12
 
-def test_tally_costs(
+
+def test_tally_costs_1(
     garden_1: np.ndarray, garden_2: np.ndarray, garden_3: np.ndarray
 ) -> None:
-    costs_1 = tally_costs(garden_1)
+    costs_1 = tally_costs_1(garden_1)
     assert [costs_1[c] for c in "ABCDE"] == [40, 32, 40, 4, 24]
-    costs_3 = tally_costs(garden_3)
+    costs_3 = tally_costs_1(garden_3)
     reference = [216, 32, 392, 180, 260, 220, 4, 234, 308, 60, 24]
     assert sum(costs_3.values()) == sum(reference)
     print(costs_3)
@@ -81,9 +100,6 @@ def test_tally_costs(
     for garden_type, cost in zip("RICFVJCEIMS", reference):
         costs_3_ref[garden_type] += cost
     assert costs_3 == costs_3_ref
-    # for a, b in zip([costs_3[c] for c in ], reference):
-    #     print(a, b)
-    # assert [costs_3[c] for c in "RICFVJCEIMS"] == reference
 
 
 def test_solve_1(
@@ -92,3 +108,17 @@ def test_solve_1(
     assert solve_1(garden_1) == 140
     assert solve_1(garden_2) == 772
     assert solve_1(garden_3) == 1930
+
+
+def test_solve_2(
+    garden_1: np.ndarray,
+    garden_2: np.ndarray,
+    garden_3: np.ndarray,
+    garden_4: np.ndarray,
+    garden_5: np.ndarray,
+) -> None:
+    assert solve_2(garden_1) == 80
+    assert solve_2(garden_2) == 436
+    assert solve_2(garden_3) == 1206
+    assert solve_2(garden_4) == 236
+    assert solve_2(garden_5) == 368

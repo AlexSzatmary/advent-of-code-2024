00de977805b3142df2330a27c077b1b164afbfe2
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-12-18 09:22:44 -0500

    Solve day-13-2

--- day-13/run_13.py
+++ day-13/run_13.py
@@ -12,7 +12,7 @@ def parse(lines: list[str]) -> list[ClawMachine]:
     return [ClawMachine(*map(int, tup)) for tup in re.findall(pattern, s)]
 
 
-def n_token(cm: ClawMachine) -> int:
+def n_token_1(cm: ClawMachine) -> int:
     det = cm.ax * cm.by - cm.bx * cm.ay
     na = (cm.by * cm.px - cm.bx * cm.py) // det
     nb = (-cm.ay * cm.px + cm.ax * cm.py) // det
@@ -23,7 +23,22 @@ def n_token(cm: ClawMachine) -> int:
 
 
 def solve_1(arcade: list[ClawMachine]) -> int:
-    return sum(map(n_token, arcade))
+    return sum(map(n_token_1, arcade))
+
+
+def n_token_2(cm: ClawMachine) -> int:
+    cm = cm._replace(px=cm.px + 10000000000000, py=cm.py + 10000000000000)
+    det = cm.ax * cm.by - cm.bx * cm.ay
+    na = (cm.by * cm.px - cm.bx * cm.py) // det
+    nb = (-cm.ay * cm.px + cm.ax * cm.py) // det
+    if cm.ax * na + cm.bx * nb == cm.px and cm.ay * na + cm.by * nb == cm.py:
+        return 3 * na + nb
+    else:
+        return 0
+
+
+def solve_2(arcade: list[ClawMachine]) -> int:
+    return sum(map(n_token_2, arcade))
 
 
 def main(argv: list[str] | None = None) -> None:
@@ -37,6 +52,8 @@ def main(argv: list[str] | None = None) -> None:
     start = timeit.default_timer()
     if "1" in argv:
         print(solve_1(stones))
+    if "2" in argv:
+        print(solve_2(stones))
     stop = timeit.default_timer()
     if "time" in argv:
         print("Time:", stop - start)
--- day-13/test_13.py
+++ day-13/test_13.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from run_13 import parse, ClawMachine, n_token, solve_1
+from run_13 import parse, ClawMachine, n_token_1, solve_1, n_token_2, solve_2
 
 
 def load(file_name: str) -> list[str]:
@@ -19,11 +19,22 @@ def test_parse(arcade_1: list[ClawMachine]) -> None:
     assert arcade_1[-1] == ClawMachine(ax=69, ay=23, bx=27, by=71, px=18641, py=10279)
 
 
-def test_n_token(arcade_1: list[ClawMachine]) -> None:
+def test_n_token_1(arcade_1: list[ClawMachine]) -> None:
     references = [280, 0, 200, 0]
     for claw_machine, reference in zip(arcade_1, references):
-        assert n_token(claw_machine) == reference
+        assert n_token_1(claw_machine) == reference
+
+
+def test_n_token_2(arcade_1: list[ClawMachine]) -> None:
+    is_nonzeros = [False, True, False, True]
+    for claw_machine, is_nonzero in zip(arcade_1, is_nonzeros):
+        assert (n_token_2(claw_machine) != 0) == is_nonzero
 
 
 def test_solve_1(arcade_1: list[ClawMachine]) -> None:
     assert solve_1(arcade_1) == 480
+
+
+def test_solve_2(arcade_1: list[ClawMachine]) -> None:
+    # This reference is pegged to what I found when I solved the example.
+    assert solve_2(arcade_1) == 875318608908

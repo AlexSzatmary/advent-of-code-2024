3a1c923157c98546d42daed5d6db2ac6fbacadb8
Alex C. Szatmary <alex.szatmary@gmail.com>
2025-01-04 22:29:33 -0500

    Solve day-15-2

--- day-15/run_15.py
+++ day-15/run_15.py
@@ -107,6 +107,190 @@ def solve_1(
     return gps(crates)
 
 
+def convert_1_to_2(
+    i_robot: int, j_robot: int, crates: np.ndarray, walls: np.ndarray, movements: str
+) -> tuple[int, int, np.ndarray, np.ndarray, str]:
+    # the east-west direction is doubled
+    # double j
+    # stretch crates and use True to indicate that the left side of a crate is on a tile
+    # (and the right side is on the tile to its right, which would be False)
+    # we simply double up the walls
+    j_robot *= 2
+    crates_2 = np.zeros((crates.shape[0], 2 * crates.shape[1]), dtype=bool)
+    crates_2[:, ::2] = crates
+    walls_2 = np.zeros((walls.shape[0], 2 * walls.shape[1]), dtype=bool)
+    walls_2[:, 0::2] = walls
+    walls_2[:, 1::2] = walls
+    return i_robot, j_robot, crates_2, walls_2, movements
+
+
+def render_2(i_robot: int, j_robot: int, crates: np.ndarray, walls: np.ndarray) -> str:
+    robot = np.zeros_like(crates)
+    robot[i_robot, j_robot] = True
+    return "\n".join(
+        "".join(
+            "["
+            if crates[i, j]
+            else "]"
+            if crates[i, j - 1]
+            else "#"
+            if walls[i, j]
+            else "@"
+            if robot[i, j]
+            else "."
+            for j in range(crates.shape[1])
+        )
+        for i in range(crates.shape[0])
+    )
+
+
+def move_2(
+    i_robot: int, j_robot: int, crates: np.ndarray, walls: np.ndarray, movements: str
+) -> tuple[int, int, np.ndarray]:
+    crates = crates.copy()
+    for m in movements:
+        m = cast(Union[Literal[">"], Literal["^"], Literal["<"], Literal["v"]], m)
+        match m:
+            case ">":
+                di, dj = (0, 1)
+            case "^":
+                di, dj = (-1, 0)
+            case "<":
+                di, dj = (0, -1)
+            case "v":
+                di, dj = (1, 0)
+
+        if crates[i_robot + di, j_robot + dj] or crates[i_robot + di, j_robot + dj - 1]:
+            # this is the complicated branch
+            if m == ">":
+                i_robot, j_robot, crates = maybe_move_crate_2_e(
+                    i_robot, j_robot, crates, walls
+                )
+            elif m == "<":
+                i_robot, j_robot, crates = maybe_move_crate_2_w(
+                    i_robot, j_robot, crates, walls
+                )
+            else:
+                i_robot, j_robot, crates = maybe_move_crate_2_ns(
+                    i_robot, j_robot, di, crates, walls
+                )
+        elif not walls[i_robot + di, j_robot + dj]:
+            i_robot += di
+            j_robot += dj
+        else:  # walls[i_robot + di, j_robot + dj]:
+            pass  # the robot hits a wall directly and does not move
+        # print(f"Move: {m}")
+        # print(render_2(i_robot, j_robot, crates, walls))
+        # print()
+        # input()
+    return i_robot, j_robot, crates
+
+
+def maybe_move_crate_2_e(
+    i_robot: int, j_robot: int, crates: np.ndarray, walls: np.ndarray
+) -> tuple[int, int, np.ndarray]:
+    # @[][].#
+    # ^^.^..#
+    # 012345#
+    j_to = j_robot + 3
+    while True:
+        if crates[i_robot, j_to]:
+            j_to += 2
+        elif not walls[i_robot, j_to]:
+            # move the crate
+            crates[i_robot, j_robot + 2 : j_to + 1] = crates[
+                i_robot, j_robot + 1 : j_to
+            ]
+            crates[i_robot, j_robot + 1] = False
+            # move the robot
+            j_robot += 1
+            break
+        else:  # wall
+            break
+    return i_robot, j_robot, crates
+
+
+def maybe_move_crate_2_w(
+    i_robot: int, j_robot: int, crates: np.ndarray, walls: np.ndarray
+) -> tuple[int, int, np.ndarray]:
+    # #.[][]@
+    # #543210
+    j_to = j_robot - 4
+    while True:
+        if crates[i_robot, j_to]:
+            j_to -= 2
+        elif not walls[i_robot, j_to + 1]:
+            # move the crate
+            crates[i_robot, j_to : j_robot - 1] = crates[i_robot, j_to + 1 : j_robot]
+            crates[i_robot, j_robot - 1] = False
+            # move the robot
+            j_robot -= 1
+            break
+        else:  # wall
+            break
+    return i_robot, j_robot, crates
+
+
+def maybe_move_crate_2_ns(
+    i_robot: int, j_robot: int, di: int, crates: np.ndarray, walls: np.ndarray
+) -> tuple[int, int, np.ndarray]:
+    # throughout, I presume we're trying to move up (di = -1) but everything reverses
+    # fine to go down.
+
+    # I'm managing wide crates by marking their left coordinate. Therefore, a crate can
+    # be:
+    # * above or above and to the left of the robot
+    # * above, above and to the left, or above and to the right of a crate. A crate can
+    # be below 0, 1, or 2 crates
+    # * below a wall, or below and to the right of a wall
+    all_crates = [
+        (i_robot + di, j) for j in [j_robot - 1, j_robot] if crates[i_robot + di, j]
+    ]  # this should select just one crate
+    crates_to_check = all_crates.copy()
+    crates_to_check_next = []
+
+    can_move = False
+
+    while True:
+        any_walls = [
+            (i + di, j)
+            for i, j_crate in crates_to_check
+            for j in [j_crate, j_crate + 1]
+            if walls[i + di, j]
+        ]
+        crates_to_check_next = {
+            (i + di, j)
+            for i, j_crate in crates_to_check
+            for j in [j_crate - 1, j_crate, j_crate + 1]
+            if crates[i + di, j]
+        }
+        crates_to_check = crates_to_check_next
+        all_crates.extend(crates_to_check)
+        if not crates_to_check_next and not any_walls:
+            can_move = True
+            break
+        elif any_walls:  # wall
+            break
+
+    if can_move:
+        # we need reversed so we move the top crates up first
+        for crate_i, crate_j in reversed(all_crates):
+            # move the crates
+            crates[crate_i, crate_j] = False
+            crates[crate_i + di, crate_j] = True
+        # move the robot
+        i_robot += di
+
+    return i_robot, j_robot, crates
+
+
+def solve_2(
+    i: int, j: int, crates: np.ndarray, walls: np.ndarray, movements: str
+) -> int:
+    i, j, crates = move_2(i, j, crates.copy(), walls, movements)
+    return gps(crates)
+
+
 def main(argv: list[str] | None = None) -> None:
     if argv is None:
         argv = sys.argv
@@ -118,6 +302,9 @@ def main(argv: list[str] | None = None) -> None:
     start = timeit.default_timer()
     if "1" in argv:
         print(solve_1(i, j, crates, walls, movements))
+    if "2" in argv:
+        i, j, crates, walls, movements = convert_1_to_2(i, j, crates, walls, movements)
+        print(solve_2(i, j, crates, walls, movements))
 
     stop = timeit.default_timer()
     if "time" in argv:
--- day-15/test_15.py
+++ day-15/test_15.py
@@ -7,7 +7,12 @@ from run_15 import (
     # gps, not yet tested explicitly; it's easier to just test solve and move
     render,
     solve_1,
+    convert_1_to_2,
+    render_2,
+    move_2,
+    solve_2,
 )
+from inspect import cleandoc
 
 
 def load(file_name: str) -> list[str]:
@@ -25,6 +30,11 @@ def crates_movements_1() -> tuple[int, int, np.ndarray, np.ndarray, str]:
 @pytest.fixture
 def crates_movements_2() -> tuple[int, int, np.ndarray, np.ndarray, str]:
     return parse(load("inex-15-2.txt"))
+
+
+@pytest.fixture
+def crates_movements_3() -> tuple[int, int, np.ndarray, np.ndarray, str]:
+    return parse(load("inex-15-3.txt"))
 
 
 def test_parse(
@@ -45,18 +55,18 @@ def test_render(
     crates_movements_1: tuple[int, int, np.ndarray, np.ndarray, str],
 ) -> None:
     i, j, crates, walls, _ = crates_movements_1
-    reference = """
-##########
-#..O..O.O#
-#......O.#
-#.OO..O.O#
-#..O@..O.#
-#O#..O...#
-#O..O..O.#
-#.OO.O.OO#
-#....O...#
-##########
-"""[1:-1]
+    reference = cleandoc("""
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+    """)
     s = render(i, j, crates, walls)
     assert s == reference
 
@@ -71,34 +81,33 @@ def test_move(
     print(render(i, j, crates, walls))
     assert (
         render(i, j, crates, walls)
-        == """
-########
-#....OO#
-##.....#
-#.....O#
-#.#O@..#
-#...O..#
-#...O..#
-########
-"""[1:-1]
+        == cleandoc("""
+        ########
+        #....OO#
+        ##.....#
+        #.....O#
+        #.#O@..#
+        #...O..#
+        #...O..#
+        ########
+        """)
     )
 
     i, j, crates, walls, movements = crates_movements_1
     i, j, crates = move(i, j, crates.copy(), walls, movements)
-    assert (
-        render(i, j, crates, walls)
-        == """
-##########
-#.O.O.OOO#
-#........#
-#OO......#
-#OO@.....#
-#O#.....O#
-#O.....OO#
-#O.....OO#
-#OO....OO#
-##########
-"""[1:-1]
+    assert render(i, j, crates, walls) == cleandoc(
+        """
+        ##########
+        #.O.O.OOO#
+        #........#
+        #OO......#
+        #OO@.....#
+        #O#.....O#
+        #O.....OO#
+        #O.....OO#
+        #OO....OO#
+        ##########
+        """
     )
 
 
@@ -110,3 +119,90 @@ def test_solve_1(
     assert solve_1(i, j, crates, walls, movements) == 10092
     i, j, crates, walls, movements = crates_movements_2
     assert solve_1(i, j, crates, walls, movements) == 2028
+
+
+def test_convert_1_to_2(
+    crates_movements_1: tuple[int, int, np.ndarray, np.ndarray, str],
+) -> None:
+    i, j, crates, walls, movements = crates_movements_1
+    i, j, crates, walls, movements = convert_1_to_2(i, j, crates, walls, movements)
+    assert i == 4
+    assert j == 8
+    assert crates.shape == (10, 20)
+    assert walls.shape == (10, 20)
+    assert np.sum(crates) == 21
+    assert np.sum(walls[2:-2, 2:-2]) == 2
+    assert len(movements) > 200  # I eyeballed it and it looked like a lot
+
+
+def test_render_2(
+    crates_movements_1: tuple[int, int, np.ndarray, np.ndarray, str],
+) -> None:
+    i, j, crates, walls, movements = crates_movements_1
+    i, j, crates, walls, _ = convert_1_to_2(i, j, crates, walls, movements)
+    reference = cleandoc("""
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##....[]@.....[]..##
+        ##[]##....[]......##
+        ##[]....[]....[]..##
+        ##..[][]..[]..[][]##
+        ##........[]......##
+        ####################
+        """)
+    s = render_2(i, j, crates, walls)
+    print(reference)
+    print()
+    print(s)
+    assert s == reference
+
+
+def test_move_2(
+    crates_movements_1: tuple[int, int, np.ndarray, np.ndarray, str],
+    crates_movements_3: tuple[int, int, np.ndarray, np.ndarray, str],
+) -> None:
+    i, j, crates, walls, movements = crates_movements_3
+    i, j, crates, walls, movements = convert_1_to_2(i, j, crates, walls, movements)
+    print(render_2(i, j, crates, walls))
+    i, j, crates = move_2(i, j, crates.copy(), walls, movements)
+    print(render_2(i, j, crates, walls))
+    assert (
+        render_2(i, j, crates, walls)
+        == cleandoc("""
+            ##############
+            ##...[].##..##
+            ##...@.[]...##
+            ##....[]....##
+            ##..........##
+            ##..........##
+            ##############
+            """)
+    )
+
+    i, j, crates, walls, movements = crates_movements_1
+    i, j, crates, walls, movements = convert_1_to_2(i, j, crates, walls, movements)
+    i, j, crates = move_2(i, j, crates.copy(), walls, movements)
+    assert render_2(i, j, crates, walls) == cleandoc(
+        """
+        ####################
+        ##[].......[].[][]##
+        ##[]...........[].##
+        ##[]........[][][]##
+        ##[]......[]....[]##
+        ##..##......[]....##
+        ##..[]............##
+        ##..@......[].[][]##
+        ##......[][]..[]..##
+        ####################
+        """
+    )
+
+
+def test_solve_2(
+    crates_movements_1: tuple[int, int, np.ndarray, np.ndarray, str],
+) -> None:
+    i, j, crates, walls, movements = crates_movements_1
+    i, j, crates, walls, movements = convert_1_to_2(i, j, crates, walls, movements)
+    assert solve_2(i, j, crates, walls, movements) == 9021
